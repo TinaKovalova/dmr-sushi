8c84d77c99185f50b2b3a19311a29d03a53a0372
TinaKovalova <kovalevatkv@gmail.com>
2025-08-16 17:11:56 +0300

    header

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,20 @@ import "/src/scss/style.scss";
 
 window.addEventListener("load", () => {
 
+    const burgerButton = document.querySelector(".burger-button");
+    const navigation = document.querySelector(".navigation");
   
+    burgerButton?.addEventListener("click", (e) => {
+        navigation?.classList.toggle("_open");
+        e.target.classList.toggle("_active");
+    })
+
+    navigation?.addEventListener("click", (e) => {
+        if (e.target.closest(".navigation__link")) {
+            burgerButton?.classList.toggle("_active");
+            e.currentTarget.classList.toggle("_open");
+        }
+    })
 
 });
 
