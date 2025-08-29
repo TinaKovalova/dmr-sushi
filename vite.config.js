ba9bc32e9aaada3fc25b0dae59c7cefc03acb3d8
TinaKovalova <kovalevatkv@gmail.com>
2025-08-29 23:11:03 +0300

    scrollbar

--- vite.config.js
+++ vite.config.js
@@ -4,26 +4,10 @@ import { defineConfig } from "vite";
 import injectHTML from "vite-plugin-html-inject";
 import VitePluginWebpAndPath from "vite-plugin-webp-and-path";
 
-
-// const __dirname = dirname(fileURLToPath(import.meta.url));
-
 export default defineConfig({
   // base:"/dmr-sushi/",
   plugins: [
     injectHTML(), VitePluginWebpAndPath()],
-  build: {
-    rollupOptions: {
-      input: {
-        // index: resolve(__dirname, "index.html"),
-        // products: resolve(__dirname, "nested/products.html"),
-        // product_details: resolve(__dirname, "nested/product-details.html"),
-        // services: resolve(__dirname, "nested/services.html"),
-        // article: resolve(__dirname, "nested/article.html"),
-        // article_detail: resolve(__dirname, "nested/article-detail.html"),
-        // about: resolve(__dirname, "nested/about.html"),
-      },
-    },
-  },
 });
 
 
