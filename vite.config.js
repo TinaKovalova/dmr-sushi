683b2e72de0ffa382e0c202235ce28d21ef7cfea
TinaKovalova <kovalevatkv@gmail.com>
2025-08-31 00:17:24 +0300

    config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
-import { dirname, resolve } from "node:path";
-import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import injectHTML from "vite-plugin-html-inject";
 import VitePluginWebpAndPath from "vite-plugin-webp-and-path";
 
 export default defineConfig({
-  // base:"/dmr-sushi/",
+  base:"/dmr-sushi/",
   plugins: [
     injectHTML(),
     VitePluginWebpAndPath()
